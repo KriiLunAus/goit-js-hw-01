8f47a1254799a23f0e19c2b8b3104ba647dec0db
KriiLunAus <kriilunaus16@gmail.com>
2023-11-17 10:51:17 +0100

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -1,9 +1,21 @@
 `use strict`
 
 function getElementWidth(content, padding, border) {
-    return `${(content) + ((padding*2)) + ((border*2))};`
+
+    content = Number(content.replace(/px/, ""));
+    padding = Number(padding.replace(/px/, ""));
+    border = Number(border.replace(/px/, ""));
+    const width = Number(content) + Number(padding)*2 + Number(border)*2;
+    return width;
+
+
+
+
+    
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
 
 //  return `${((content.replace(/px/, ""))) + ((padding.replace(/px/, "")*2)) + ((border.replace(/px/, "")*2))};`
